242423beecebf0f7d26d4fa10f1265d7f08c4832
NetFound <prosto5564821@gmail.com>
2020-02-24 19:14:18 +0300

    init commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+
+        <header clasName="header">
+            <img src="https://s1.logaster.com/static/v3/img/products/logo.png"/>
+        </header>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+
+const Profile = () => {
+    return (
+
+        <div className="content">
+            <div>
+                <img src="http://www.cgchannel.com/wp-content/uploads/2018/07/180704_WDASMoanaIslandScene.jpg"  />
+            </div>
+            <div className="avatar">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQW484a-ern0v95PEE0bbW89ygmXgncz7utzI7Ayp1fykchvCBI"/>
+            </div>
+            <div/>
+            <div>
+                ava+discriptin
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New posts
+                </div>
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+                <div>
+                    post3
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
